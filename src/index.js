94bdadcb3982587055d0b443b62de5f348985873
Arthur <arthurgomesferreira.agf@gmail.com>
2023-05-13 10:36:13 -0300

    New pages rendered

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import { createRoot } from "react-dom/client";
 
 // import App from "./App";
 import ConsultaUsuarios from "./pages/ConsultaUsuarios/index";
+import ConsultaPostagens from "./pages/ConsultaPostagens/index";
+import ConsultaMaterias from "./pages/ConsultaMaterias/index";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <ConsultaUsuarios />
+	  <ConsultaUsuarios />
+	  <ConsultaMaterias />
+	  <ConsultaPostagens />
   </StrictMode>
 );
